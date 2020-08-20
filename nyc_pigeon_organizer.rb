c44ba9408c34d37078b8081f1edc78140e616c67
Kevin Iturralde <keviniturralde3@gmail.com>
2020-08-20 15:26:13 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,5 +9,10 @@ def nyc_pigeon_organizer(data)
         if !new_hash [name][key]
           new_hash [name][key] = []
         end
+        new_hash [name][key] << new_value.to_s
+      end
+    end
+  end
+  new_hash
   # write your code here!
 end
